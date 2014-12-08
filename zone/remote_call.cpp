a5acaf0f18a7cdbc62ebab8d42efbd4bc86ff207
Akkadius <akkadius1@gmail.com>
2014-12-08 05:38:52 -0600

    Add "size" to Zone.SetEntityAttribute

--- zone/remote_call.cpp
+++ zone/remote_call.cpp
@@ -327,5 +327,6 @@ void handle_rc_set_entity_attribute(const std::string &method, const std::string
 		if (params[1] == "race"){ ent->SendIllusionPacket(atoi(params[2].c_str())); }
 		if (params[1] == "appearance_effect"){ ent->SendAppearanceEffect(atoi(params[2].c_str()), 0, 0, 0, 0); }
 		if (params[1] == "heading"){ ent->GMMove(ent->GetX(), ent->GetY(), ent->GetZ(), atoi(params[2].c_str()), true); }
+		if (params[1] == "size"){ ent->ChangeSize(atoi(params[2].c_str())); }
 	}
 }
