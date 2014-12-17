6ef4a46d7a48c19a471a49f23a396a349cb28770
Michael Cook (mackal) <mcook@mackal.net>
2014-12-17 01:52:00 -0500

    Switch path stuff to vectors
    This should be more CPU cache friendly compared to lists so should
    be faster. (profiling looked promising)
    
    Conflicts:
    	changelog.txt
    	zone/client_packet.cpp
    	zone/pathing.h

--- zone/mob.h
+++ zone/mob.h
@@ -1200,7 +1200,7 @@ protected:
 	Map::Vertex PathingLastPosition;
 	int PathingLoopCount;
 	int PathingLastNodeVisited;
-	std::list<int> Route;
+	std::vector<int> Route;
 	LOSType PathingLOSState;
 	Timer *PathingLOSCheckTimer;
 	Timer *PathingRouteUpdateTimerShort;
