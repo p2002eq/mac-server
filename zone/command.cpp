49c6ffb926c194cfd2b832f45471029d1417949c
stheno <turbod_speedy@hotmail.com>
2014-12-30 01:53:15 -0800

    Fixed command note and status level of last commit.

--- zone/command.cpp
+++ zone/command.cpp
@@ -330,7 +330,7 @@ int command_init(void){
 		command_add("qglobal", "[on/off/view] - Toggles qglobal functionality on an NPC", 100, command_qglobal) ||
 		command_add("qtest", "- QueryServ testing command.", 255, command_qtest) ||
 		command_add("questerrors", "Shows quest errors.", 100, command_questerrors) ||
-		command_add("questupdate", "Shows quest errors.", 100, command_questupdate) ||
+		command_add("questupdate", "Updates the quest folder if it is tied to an svn.", 250, command_questupdate) ||
 
 		command_add("race", "[racenum] - Change your or your target's race. Use racenum 0 to return to normal", 50, command_race) ||
 		command_add("raidloot", "LEADER|GROUPLEADER|SELECTED|ALL - Sets your raid loot settings if you have permission to do so.", 0, command_raidloot) ||
