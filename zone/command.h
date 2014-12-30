fd8fe328ff2358610cba4cf7e68195267071af5a
stheno <turbod_speedy@hotmail.com>
2014-12-30 01:29:37 -0800

    Implemented #questupdate If you are default status 250 or higher you can issue this on the server if the quest folder is tied to a svn.

--- zone/command.h
+++ zone/command.h
@@ -318,6 +318,7 @@ void command_npctype_cache(Client *c, const Seperator *sep);
 void command_starve(Client *c, const Seperator *sep);
 void command_merchantopenshop(Client *c, const Seperator *sep);
 void command_merchantcloseshop(Client *c, const Seperator *sep);
+void command_questupdate(Client *c, const Seperator *sep);
 
 #ifdef EQPROFILE
 void command_profiledump(Client *c, const Seperator *sep);
