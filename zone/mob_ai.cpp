c719dd85724ed58b8485a58ea54ef76edb8ec8a3
Akkadius <akkadius1@gmail.com>
2015-01-20 01:41:47 -0600

    Remove Duplicative MySQL Error: Error in GetMaxNPCSpellsEffectsID query '

--- zone/mob_ai.cpp
+++ zone/mob_ai.cpp
@@ -2914,7 +2914,6 @@ uint32 ZoneDatabase::GetMaxNPCSpellsEffectsID() {
 	std::string query = "SELECT max(id) FROM npc_spells_effects";
 	auto results = QueryDatabase(query);
 	if (!results.Success()) {
-        std::cerr << "Error in GetMaxNPCSpellsEffectsID query '" << query << "' " << results.ErrorMessage() << std::endl;
 		return 0;
 	}
 
