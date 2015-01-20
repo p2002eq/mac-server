02d1ab373edafba617ab6ee826dca85635f8bf4d
Akkadius <akkadius1@gmail.com>
2015-01-20 01:42:22 -0600

    Remove Duplicative MySQL Error: Error in GetPEQZone query '

--- common/database.cpp
+++ common/database.cpp
@@ -1158,7 +1158,6 @@ uint8 Database::GetPEQZone(uint32 zoneID, uint32 version){
 
 	if (!results.Success())
 	{
-		std::cerr << "Error in GetPEQZone query '" << query << "' " << results.ErrorMessage() << std::endl;
 		return 0;
 	}
 
