edeb64873ee0cb12b12941212200ecbec0e4d2ba
cavedude00 <cavedude@peqtgc.com>
2015-01-19 19:17:39 -0800

    /bug is implemented for Intel clients. Moved the decode to a native handler. Intel will send all the info PC does, except for type as there is no support for that in the client.

--- zone/zonedb.h
+++ zone/zonedb.h
@@ -335,7 +335,7 @@ public:
 	NPCType*					    GetNPCTypeTemp(uint32 id);
 
 	/* Petitions   */
-	void	UpdateBug(BugStruct* bug);
+	void	UpdateBug(BugStruct* bug, uint32 clienttype);
 	void	UpdateFeedback(Feedback_Struct* feedback);
 	void	DeletePetitionFromDB(Petition* wpet);
 	void	UpdatePetitionToDB(Petition* wpet);
