5c459016a0f57c854d220c1a351fa771cb43399f
Akkadius <akkadius1@gmail.com>
2015-01-20 19:41:24 -0600

    Remove Unneeded DebugBreak

--- zone/entity.cpp
+++ zone/entity.cpp
@@ -167,7 +167,6 @@ const Client *Entity::CastToClient() const
 #ifdef _EQDEBUG
 	if (!IsClient()) {
 		std::cout << "CastToClient error (not client?)" << std::endl;
-		DebugBreak();
 		return 0;
 	}
 #endif
