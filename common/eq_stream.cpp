0802fc0b5e5f15dde2ec0ba4c5103186757d408b
SecretsOTheP <espause0703@gmail.com>
2015-01-23 21:40:18 -0500

    revert till a better solution is found

--- common/eq_stream.cpp
+++ common/eq_stream.cpp
@@ -1470,6 +1470,11 @@ FragmentGroup::FragmentGroup(uint16 seq, uint16 opcode, uint16 num_fragments)
 
 FragmentGroup::~FragmentGroup()
 {
+	for(int i=0;i<num_fragments;i++)
+	{
+		if(&fragment[i])
+			delete &fragment[i];
+	}
 	safe_delete_array(fragment);//delete[] fragment;
 }
 
@@ -1478,6 +1483,7 @@ void FragmentGroup::Add(uint16 frag_id, uchar* data, uint32 size)
 	//The frag_id references a fragment within the group
 	if(frag_id < num_fragments)
 	{
+		if(fragment)
 		fragment[frag_id].SetData(data, size);
 	}
 	//The frag_id is attempting to reference an element outside the bounds of the group array
@@ -1906,7 +1912,6 @@ bool EQOldStream::ProcessPacket(EQOldPacket* pack, bool from_buffer)
 			EQRawApplicationPacket *app = new EQRawApplicationPacket(fragment_group->GetOpcode(), buf, sizep);
 			safe_delete_array(buf);
 			OutQueue.push_back(app);
-			safe_delete(fragment_group);
 			return true;
 		}
 		else
