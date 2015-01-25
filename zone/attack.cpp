2e4a25d4b04ae37f2c40045bc0fd1041344e98b3
regneq <rregenn@gmail.com>
2015-01-24 19:05:39 -0800

    fixed combat logging.

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -17,6 +17,7 @@
 */
 
 #include "../common/debug.h"
+#include "../common/eqemu_logsys.h"
 #include "../common/eq_constants.h"
 #include "../common/eq_packet_structs.h"
 #include "../common/rulesys.h"
@@ -1357,7 +1358,7 @@ void Mob::AggroPet(Mob* attacker)
 		if (pet && !pet->IsFamiliar() && !pet->GetSpecialAbility(IMMUNE_AGGRO) && !pet->IsEngaged() && attacker && attacker != this && !attacker->IsCorpse())
 		{
 			if (!pet->IsHeld()) {
-				mlog(PETS__AGGRO, "Sending pet %s into battle due to attack.", pet->GetName());
+				logger.DebugCategory(EQEmuLogSys::Detail, EQEmuLogSys::Combat, "Sending pet %s into battle due to attack.", pet->GetName());
 				pet->AddToHateList(attacker, 1);
 				pet->SetTarget(attacker);
 				Message_StringID(10, PET_ATTACKING, pet->GetCleanName(), attacker->GetCleanName());
