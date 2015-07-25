c59952d2b698fdc850942946bfbb0f33984f555f
Stheno <turbod_speedy@hotmail.com>
2015-07-25 11:52:52 -0700

    Got further with the sql fails. Still an issue with entering the insert even when data exists.

--- loginserver/database.cpp
+++ loginserver/database.cpp
@@ -398,69 +398,100 @@ bool Database::CreateServerSettings()
 
 	string check_table_query = "SHOW TABLES LIKE 'tblloginserversettings'";
 
-	if (mysql_query(db, check_table_query.c_str()) == 0)
-	{
-		//no idea why this fails.
-		server_log->Log(log_debug, "Server Settings table does not exist, creating.");
-		string create_table_query =
-			"CREATE TABLE `tblloginserversettings` ("
-			"`type` varchar(50) NOT NULL,"
-			"`value` varchar(50) NOT NULL,"
-			"`category` varchar(20) NOT NULL,"
-			"`description` varchar(99) NOT NULL,"
-			"`defaults` varchar(50) NOT NULL,"
-			"PRIMARY KEY(`type`)"
-			") ENGINE = InnoDB DEFAULT CHARSET = latin1";
-
-		if (mysql_query(db, create_table_query.c_str()) != 0)
-		{
-			server_log->Log(log_error, "Mysql query failed: %s", create_table_query.c_str());
-			return false;
-		}
-		server_log->Log(log_database, "Server Settings table created, continuing.");
-	}
-
 	if (mysql_query(db, check_table_query.c_str()) != 0)
 	{
-		server_log->Log(log_database, "Server Settings tables exist, continuing.");
+		server_log->Log(log_error, "Mysql query failed: %s", check_table_query.c_str());
+		return false;
 	}
 
-	//string check_settings_query = "SELECT * FROM `tblloginserversettings` WHERE `type`";
+	res = mysql_use_result(db);
+	if (res)
+	{
+		if ((row = mysql_fetch_row(res)) == nullptr)
+		{
+			server_log->Log(log_debug, "Server Settings table does not exist, creating.");
 
-	//if (mysql_query(db, check_settings_query.c_str()) != 0)
-	//{
-	//	server_log->Log(log_database, "Server Settings table exists but settings tables do not, creating settings entries.");
-	//	string query =
-	//		"INSERT INTO `tblloginserversettings` VALUES ('access_log_table', '', 'schema', 'location for access logs, failed logins and goodIP.', 'tblaccountaccesslog');"
-	//		"INSERT INTO `tblloginserversettings` VALUES('account_table', '', 'schema', 'location of all client account info for login server only.', 'tblLoginServerAccounts');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('auto_account_activate', '', 'options', 'set this to TRUE to allow new accounts to log in.', 'TRUE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('auto_account_create', '', 'options', 'set this to TRUE to auto create accounts on player first log in.', 'TRUE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('clientport', '', 'Old', 'port for clients to connect to.', '6000');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('dump_packets_in', '', 'options', 'debugging', 'FALSE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('dump_packets_out', '', 'options', 'debugging', 'FALSE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('failed_login_log', '', 'options', 'set this to TRUE to log failed log in attempts.', 'TRUE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('good_loginIP_log', '', 'options', 'set this to TRUE to log successful account log ins.', 'TRUE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('listen_port', '', 'options', 'this is the port we listen on for world connection.', '5998');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('local_network', '', 'options', 'set to the network ip that world server is on.', '127.0.0.1');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('mode', '', 'security', 'encryption mode the plugin uses.', '5');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('network_ip', '', 'options', 'set to the network ip that world server is on.', '127.0.0.1');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('opcodes', '', 'Old', 'opcode file for client compatibility. (Old means classic/mac)', 'login_opcodes_oldver.conf');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('plugin', '', 'security', 'the encryption type the login server uses.', 'EQEmuAuthCrypto');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('reject_duplicate_servers', '', 'options', 'set this to TRUE to force unique server name connections.', 'TRUE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('salt', '', 'options', 'for account security make this a numeric random number.', '12345678');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('trace', '', 'options', 'debugging', 'FALSE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('unregistered_allowed', '', 'options', 'set this to TRUE to allow any server to connect.', 'TRUE');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('world_admin_registration_table', '', 'schema', 'location of administrator account info for this login server.', 'tblServerAdminRegistration');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('world_registration_table', '', 'schema', 'location of registered or connection records of servers connecting to this loginserver.', 'tblWorldServerRegistration');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('world_server_type_table', '', 'schema', 'location of server type descriptions.', 'tblServerListType');"
-	//		"INSERT INTO `tblloginserversettings` VALUES ('world_trace', '', 'options', 'debugging', 'FALSE');";
+			string create_table_query =
+				"CREATE TABLE `tblloginserversettings` ("
+				"`type` varchar(50) NOT NULL,"
+				"`value` varchar(50) NOT NULL,"
+				"`category` varchar(20) NOT NULL,"
+				"`description` varchar(99) NOT NULL,"
+				"`defaults` varchar(50) NOT NULL,"
+				"PRIMARY KEY(`type`)"
+				") ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
-	//	if (mysql_query(db, query.c_str()) != 0)
-	//	{
-	//		server_log->Log(log_error, "Mysql query failed: %s", query.c_str());
-	//		return false;
-	//	}
-	//}
+			if (mysql_query(db, create_table_query.c_str()) != 0)
+			{
+				server_log->Log(log_error, "Mysql query failed: %s", create_table_query.c_str());
+				return false;
+			}
+			server_log->Log(log_database, "Server Settings table created, continuing.");
+		}
+		//doesn't work
+		else if ((row = mysql_fetch_row(res)) != nullptr)
+		{
+			server_log->Log(log_database, "Server Settings tables exists, continuing.");
+		}
+		mysql_free_result(res);
+	}
+
+	string check_settings_query = "SELECT * FROM `tblloginserversettings` WHERE `type`";
+
+	if (mysql_query(db, check_settings_query.c_str()) != 0)
+	{
+		server_log->Log(log_error, "Mysql query failed: %s", check_table_query.c_str());
+		return false;
+	}
+
+	res = mysql_use_result(db);
+	if (res)
+	{
+		// if exists it still enters this
+		if ((row = mysql_fetch_row(res)) == nullptr)
+		{
+			server_log->Log(log_database, "Server Settings table exists but settings tables do not, creating settings entries.");
+
+			string query =
+				"INSERT INTO `tblloginserversettings` (type, value, category, description, defaults)"
+				"VALUES"
+				"('access_log_table', '', 'schema', 'location for access logs, failed logins and goodIP.', 'tblaccountaccesslog'),"
+				"('account_table', '', 'schema', 'location of all client account info for login server only.', 'tblLoginServerAccounts'),"
+				"('auto_account_activate', '', 'options', 'set this to TRUE to allow new accounts to log in.', 'TRUE'),"
+				"('auto_account_create', '', 'options', 'set this to TRUE to auto create accounts on player first log in.', 'TRUE'),"
+				"('clientport', '', 'Old', 'port for clients to connect to.', '6000'),"
+				"('dump_packets_in', '', 'options', 'debugging', 'FALSE'),"
+				"('dump_packets_out', '', 'options', 'debugging', 'FALSE'),"
+				"('failed_login_log', '', 'options', 'set this to TRUE to log failed log in attempts.', 'TRUE'),"
+				"('good_loginIP_log', '', 'options', 'set this to TRUE to log successful account log ins.', 'TRUE'),"
+				"('listen_port', '', 'options', 'this is the port we listen on for world connection.', '5998'),"
+				"('local_network', '', 'options', 'set to the network ip that world server is on.', '127.0.0.1'),"
+				"('mode', '', 'security', 'encryption mode the plugin uses.', '5'),"
+				"('network_ip', '', 'options', 'set to the network ip that world server is on.', '127.0.0.1'),"
+				"('opcodes', '', 'Old', 'opcode file for client compatibility. (Old means classic/mac)', 'login_opcodes_oldver.conf'),"
+				"('plugin', '', 'security', 'the encryption type the login server uses.', 'EQEmuAuthCrypto'),"
+				"('reject_duplicate_servers', '', 'options', 'set this to TRUE to force unique server name connections.', 'TRUE'),"
+				"('salt', '', 'options', 'for account security make this a numeric random number.', '12345678'),"
+				"('trace', '', 'options', 'debugging', 'FALSE'),"
+				"('unregistered_allowed', '', 'options', 'set this to TRUE to allow any server to connect.', 'TRUE'),"
+				"('world_admin_registration_table', '', 'schema', 'location of administrator account info for this login server.', 'tblServerAdminRegistration'),"
+				"('world_registration_table', '', 'schema', 'location of registered or connection records of servers connecting to this loginserver.', 'tblWorldServerRegistration'),"
+				"('world_server_type_table', '', 'schema', 'location of server type descriptions.', 'tblServerListType'),"
+				"('world_trace', '', 'options', 'debugging', 'FALSE');";
+			;
+			if (mysql_query(db, query.c_str()) != 0)
+			{
+				server_log->Log(log_error, "Mysql query failed: %s", query.c_str());
+				return false;
+			}
+		}
+		//doesn't work
+		else if ((row = mysql_fetch_row(res)) != nullptr)
+		{
+			server_log->Log(log_database, "Server Settings entries exist, continuing.");
+		}
+		mysql_free_result(res);
+	}
 	return true;
 }
 #pragma endregion
