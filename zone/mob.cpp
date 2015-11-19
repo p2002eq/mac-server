3ef17a1507a48e9245b20d621eb1c55b47b703df
ngdeao <ngdeao@hotmail.com>
2015-11-19 15:16:40 -0700

    Charm fixes - again.

--- zone/mob.cpp
+++ zone/mob.cpp
@@ -2254,7 +2254,7 @@ bool Mob::RemoveFromHateList(Mob* mob)
 			}
 		}
 	}
-	if(GetTarget() == mob)
+	if(GetTarget() == mob && (!IsClient() || IsAIControlled()))
 	{
 		SetTarget(hate_list.GetTop());
 	}
