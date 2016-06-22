e7f158c01479ac1d4e54f5724373564af865a55a
Scott Davis <me@sdavis.info>
2016-06-21 21:43:07 -0400

    fix blind on raid targets

--- zone/bonuses.cpp
+++ zone/bonuses.cpp
@@ -2051,6 +2051,10 @@ void Mob::ApplySpellsBonuses(uint16 spell_id, uint8 casterlevel, StatBonuses* ne
 				break;
 
 			case SE_Blind:
+                if (IsRaidTarget()) {
+                    // do not blind raid targtets
+                    break;
+                }
 				new_bonus->IsBlind = true;
 				break;
 
